0beffae9cad29ed73da04739a3474e9a4ba772bd
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-21 16:44:04 +0100

    Ho aggiunto l'immagine del primo memebro

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 console.log('JS OK!')
 
-/*Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: *Nome, Ruolo e Foto*.
+/*Aggiungere un file js in cui definire *un array di oggetti* che rappresentano 
+i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa 
+card: *Nome, Ruolo e Foto*.
 Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, 
 stampare dinamicamente una card per ogni membro del team.
 BONUS:
@@ -12,7 +14,7 @@ e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
  */
 
 //per prima cosa prendo il container delle card
-const cardsContainer = document.querySelector('.team-card');
+const cardsContainer = document.querySelector('.team-container');
 
 
 // creo un array di oggetti che rappresentano ogni membro del team
@@ -20,28 +22,28 @@ const cardsContainer = document.querySelector('.team-card');
 const teamMembers = [
     //oggetto 0
     {
-        nome: 'Sandro',
-        ruolo: 'Manager',
-        foto: 'image-1'
+        nome: 'Edoardo Magro',
+        ruolo: 'Office Manager',
+        foto: 'img/walter-gordon-office-manager.jpg'
     },
 
     //oggetto 1
     {
-        nome: 'Luisa',
+        nome: 'Agnese Innocenti',
         ruolo: 'impiegata',
         foto: 'image-2'
     },
 
     //oggetto 2
     {
-        nome: 'Marina',
+        nome: 'Giammarco Tocco',
         ruolo: 'magazziniera',
         foto: 'image-3'
     },
      
     //oggetto 3
     {
-        nome: 'Dario',
+        nome: 'Dario Moccia',
         ruolo: 'streamer',
         foto: 'image-4'
     }
@@ -49,3 +51,45 @@ const teamMembers = [
 ];
 
 console.log(teamMembers);
+
+// definisco gli oggetti con delle costanti
+
+const edoMagro = teamMembers[0];
+
+const agneseInno = teamMembers[1];
+
+const giammaTocco = teamMembers[2];
+
+const darioMoccia = teamMembers[3];
+
+// una volta definiti i membri all'interno dell'array,
+// usando le informazioni inserite le aggiungo all'html per creare la card
+
+// creo un div dove inserire la card
+
+const teamCardDiv = document.createElement('div');
+
+teamCardDiv.classList.add('team-card');
+
+// lo inserisco nel container della card
+cardsContainer.appendChild(teamCardDiv);
+
+// creo dei div figli all'interno del contenitore che divide i 3 parametri
+
+const imageCardDiv = document.createElement('div');
+
+  // gli aggiungo una classe chiamata 'card-image'
+imageCardDiv.className = 'card-image';
+
+teamCardDiv.appendChild(imageCardDiv);
+
+// creo l'elemento dell'immagine
+const imageElement = document.createElement('img');
+
+console.log(imageElement)
+
+imageElement.src = edoMagro.foto;
+
+// aggiungo all'interno del div l'immagine che serve
+
+imageCardDiv.appendChild(imageElement);
